4cef360e35c3700212b499df3e67afbb55eb103d
Ryan Luong <lumintriet@gmail.com>
2025-11-28 20:24:38 +0700

    feat(next-js-demo/auth): sign up page

---
+++ demos/next-js/src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,9 @@
+import { SignUpForm } from "@/features/auth/components/sign-up-form";
+
+export default function SignUpPage() {
+  return (
+    <div className="flex h-full items-center justify-center p-6 md:p-10">
+      <SignUpForm />
+    </div>
+  );
+}
