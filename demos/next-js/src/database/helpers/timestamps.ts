a733386acb2b9037abe1282587aee63d542d7204
Ryan Luong <lumintriet@gmail.com>
2025-11-28 10:04:04 +0700

    refactor(next-js-demo/db): change js date sqlite helper name

--- demos/next-js/src/database/helpers/timestamps.ts
+++ demos/next-js/src/database/helpers/timestamps.ts
@@ -1,17 +1,17 @@
 import { integer } from "drizzle-orm/sqlite-core";
 import { sql } from "drizzle-orm";
 
-const jsDateInSqlite = sql`(unixepoch('now', 'subsec') * 1000)`;
+const jsDateNowInSqlite = sql`(unixepoch('now', 'subsec') * 1000)`;
 
 const timestamps = {
   updatedAt: integer("updated_at", { mode: "timestamp" })
     .notNull()
-    .default(jsDateInSqlite)
+    .default(jsDateNowInSqlite)
     .$onUpdate(() => new Date()),
 
   createdAt: integer("created_at", { mode: "timestamp" })
     .notNull()
-    .default(jsDateInSqlite),
+    .default(jsDateNowInSqlite),
 };
 
-export { jsDateInSqlite, timestamps };
+export { jsDateNowInSqlite, timestamps };
