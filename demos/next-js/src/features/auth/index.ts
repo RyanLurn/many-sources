187c9c01fda2a6c19fc5f8010b3f11336b5bc0a8
Ryan Luong <lumintriet@gmail.com>
2025-11-28 10:57:49 +0700

    feat(next-js-demo/auth): configure Better Auth instance with database adapter and plugin for Next.js

--- demos/next-js/src/features/auth/index.ts
+++ demos/next-js/src/features/auth/index.ts
@@ -1,5 +1,34 @@
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { nextCookies } from "better-auth/next-js";
 import { betterAuth } from "better-auth";
 
-const auth = betterAuth({});
+import {
+  verificationTable,
+  accountTable,
+  sessionTable,
+  userTable,
+} from "@/database/schema/auth";
+import { serverEnvironmentVariables } from "@/lib/env/server";
+import { database } from "@/database/connection";
+
+const auth = betterAuth({
+  database: drizzleAdapter(database, {
+    schema: {
+      verification: verificationTable,
+      session: sessionTable,
+      account: accountTable,
+      user: userTable,
+    },
+    provider: "sqlite",
+  }),
+  advanced: {
+    database: {
+      generateId: false,
+    },
+  },
+  secret: serverEnvironmentVariables.BETTER_AUTH_SECRET,
+  baseURL: serverEnvironmentVariables.BETTER_AUTH_URL,
+  plugins: [nextCookies()],
+});
 
 export { auth };
