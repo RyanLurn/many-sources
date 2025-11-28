e678a87c9b39752e7e905943364ad223c188ea85
Ryan Luong <lumintriet@gmail.com>
2025-11-28 09:10:11 +0700

    feat(next-js-demo/ui): add dark mode support

--- demos/next-js/src/app/page.tsx
+++ demos/next-js/src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <main className="flex h-dvh items-center justify-center">
+    <main className="flex h-full items-center justify-center">
       <h1>Many Sources Next.js Demo</h1>
     </main>
   );
