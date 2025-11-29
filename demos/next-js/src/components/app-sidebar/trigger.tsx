f90898b473bb2ffaa041d50dc62da1e97441ca56
Ryan Luong <lumintriet@gmail.com>
2025-11-29 20:10:39 +0700

    feat(next-js-demo/ui): AppSidebarTrigger component

---
+++ demos/next-js/src/components/app-sidebar/trigger.tsx
@@ -0,0 +1,22 @@
+import { PanelLeftOpen } from "lucide-react";
+
+import { useSidebar } from "@/components/ui/sidebar";
+import { Button } from "@/components/ui/button";
+
+function AppSidebarTrigger() {
+  const { toggleSidebar, open } = useSidebar();
+
+  if (open) {
+    return;
+  }
+
+  return (
+    <div className="fixed top-2 left-2">
+      <Button onClick={toggleSidebar} variant="outline" size="icon">
+        <PanelLeftOpen className="size-5" />
+      </Button>
+    </div>
+  );
+}
+
+export default AppSidebarTrigger;
