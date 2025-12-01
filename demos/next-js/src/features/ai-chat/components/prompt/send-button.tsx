3c4fc934b429abdd97c4211cd0db068768dfed5f
Ryan Luong <lumintriet@gmail.com>
2025-12-01 07:58:05 +0700

    feat(next-js-demo/ui): SendPromptButton component

---
+++ demos/next-js/src/features/ai-chat/components/prompt/send-button.tsx
@@ -0,0 +1,51 @@
+import type { ComponentProps } from "react";
+import type { ChatStatus } from "ai";
+
+import { Loader2Icon, SquareIcon, SendIcon, XIcon } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utilities";
+
+interface SendPromptButtonProperties extends ComponentProps<typeof Button> {
+  chatStatus: ChatStatus;
+}
+
+function SendPromptButton({
+  chatStatus,
+  className,
+  disabled,
+  ...properties
+}: SendPromptButtonProperties) {
+  let Icon = <SendIcon className="size-4" />;
+
+  switch (chatStatus) {
+    case "submitted": {
+      Icon = <Loader2Icon className="size-4 animate-spin" />;
+      break;
+    }
+    case "streaming": {
+      Icon = <SquareIcon className="size-4" />;
+      break;
+    }
+    case "error": {
+      Icon = <XIcon className="size-4" />;
+      break;
+    }
+    default: {
+      Icon = <SendIcon className="size-4" />;
+      break;
+    }
+  }
+
+  return (
+    <Button
+      className={cn("gap-1.5 rounded-lg", className)}
+      disabled={disabled}
+      {...properties}
+    >
+      {Icon}
+    </Button>
+  );
+}
+
+export { SendPromptButton };
